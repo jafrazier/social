63521653ce34a8dcfef3cbd89250f3250c6fca3e
jfrazier <johnfrazier1222@gmail.com>
2018-07-10 10:46:31 -0400

    used javascript to create divs

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+totalUsers= 0;
+users = {}
+
 var d = new Date();
 var postCount = 0;
 post = {
@@ -6,35 +9,34 @@ post = {
   dateAndTime: '',
 };
 
-function newPost(postCount){
-  post.userName = prompt('That is you user name?');
-  post.message = prompt('What would you like to say?');
-  post.dateAndTime = d;
-  document.getElementById('name').innerHTML = post.userName;
-  document.getElementById('message').innerHTML = post.message;
-  document.getElementById('date').innerHTML = post.dateAndTime;
-  postCount = postCount + 1;
-
-  }
-// counter variable
-// user object
-// has to have name of user[count]
-
-
-
-
-totalUsers= 0;
-users = {}
-
-function createUser(name, email) {
-
+function newDiv() {
+  var name = prompt('What is your User name?');
+  var email = prompt('What is your Email?')
+  var bio = prompt('Tell us more about you')
   totalUsers += 1;
   propertyName = 'user' + totalUsers;
 
   users[totalUsers] = {
     name: name,
     email: email,
+    bio : bio,
     id: totalUsers
     }
 
+  post.message = prompt('What would you like to say?');
+  post.dateAndTime = d;
+  var div = document.createElement('div');
+  div.className = 'post-content';
+  var h = document.createElement('h3');
+  h.innerHTML = users[totalUsers].name;
+  div.appendChild(h);
+  var p = document.createElement('p');
+  p.innerHTML = post.message;
+  div.appendChild(p);
+  var date = document.createElement('small');
+  date.innerHTML = post.dateAndTime;
+  div.appendChild(date);
+  var space = document.createElement('hr');
+  div.appendChild(space);
+  document.getElementById('post-display').appendChild(div);
 }
